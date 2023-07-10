01fc50a8d44e3553a174ef07d323adae6a7d460a
Candela Brassesco <candebrassesco@gmail.com>
2023-07-10 17:48:15 -0300

    Clase ProductManager creada

--- entregaClase2/clase2.js
+++ entregaClase2/clase2.js
@@ -5,23 +5,36 @@ class ProductManager {
         this.products = []
     }
 
-    addProduct (title, description, price, thumbnail, code, stock) {
+    addProduct (title, description, price, thumbnail, newCode, stock) {
+
+        // ID autoincrementable.
         const id =
           this.products.lenght === 0 ? 1
           : this.products[this.products.length - 1].id + 1
+        
+        // cargo en la variable product el   
+        const product = this.products.find(p => p.code === newCode)
 
         const newProduct = {
             id,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
+            title: title,
+            description: description,
+            price: price,
+            thumbnail: thumbnail,
+            code: newCode,
+            stock: stock
         }
-        
-    
 
+        // verificación del ingreso de todos los parámetros y unicidad del code establecido.
+
+        if ((title || description || price || thumbnail || code || stock) === undefined ) {
+            console.log('Please enter all the parameters to add a new product: title, description, price, thumbnail, code ande stock')
+        } else if (product) {
+            console.log(`The code: ${newCode} already exists. Please enter another one!`)
+        } else {
+            // Si se pasan las dos verificaciones anteriores se suma al array product el nuevo producto.
+            this.products.push(newProduct)
+        }   
     }
 
     getProduct () {
@@ -29,12 +42,14 @@ class ProductManager {
     }
 
     getProductById (idProduct) {
+        //se busca la existencia de un 
         const product = this.products.find(p => p.id === idProduct)
 
         if (!product){
             console.log('Not found')
         } else {
-            
+            return product
         }
     }
 }
+
