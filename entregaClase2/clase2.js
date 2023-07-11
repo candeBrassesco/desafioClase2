2209247458c2e8e3080ebeb9c20a0a9098ed0a2a
Candela Brassesco <candebrassesco@gmail.com>
2023-07-11 17:51:19 -0300

    ejemplos para probar las funciones

--- entregaClase2/clase2.js
+++ entregaClase2/clase2.js
@@ -5,32 +5,33 @@ class ProductManager {
         this.products = []
     }
 
-    addProduct (title, description, price, thumbnail, newCode, stock) {
+    addProduct (title, description, price, thumbnail, code, stock) {
 
         // ID autoincrementable.
         const id =
-          this.products.lenght === 0 ? 1
+          this.products.length === 0 
+          ? 1
           : this.products[this.products.length - 1].id + 1
         
-        // cargo en la variable product el   
-        const product = this.products.find(p => p.code === newCode)
+        const codeExist = this.products.find(p => p.code === code)
 
         const newProduct = {
-            id,
-            title: title,
-            description: description,
-            price: price,
-            thumbnail: thumbnail,
-            code: newCode,
-            stock: stock
+            id: id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
         }
 
         // verificación del ingreso de todos los parámetros y unicidad del code establecido.
+        // para luego agilizar los ejemplos los errores serán mostrados en consola con un console.log y no con un throw new Error.
 
-        if ((title || description || price || thumbnail || code || stock) === undefined ) {
-            console.log('Please enter all the parameters to add a new product: title, description, price, thumbnail, code ande stock')
-        } else if (product) {
-            console.log(`The code: ${newCode} already exists. Please enter another one!`)
+        if (title === undefined || description === undefined || price === undefined || thumbnail === undefined || code === undefined || stock === undefined) {
+             console.log('ERROR! Please enter all the parameters to add a new product: title, description, price, thumbnail, code and stock')
+        } else if (codeExist) {
+             console.log(`ERROR! The code: ${code} already exists. Please enter another one!`)
         } else {
             // Si se pasan las dos verificaciones anteriores se suma al array product el nuevo producto.
             this.products.push(newProduct)
@@ -46,10 +47,45 @@ class ProductManager {
         const product = this.products.find(p => p.id === idProduct)
 
         if (!product){
-            console.log('Not found')
+            throw new Error('Not found')
         } else {
             return product
         }
     }
 }
 
+
+//ejemplos de prueba:
+
+
+// 1RO: se carga todo correctamente
+
+const productManager1 = new ProductManager ()
+
+productManager1.addProduct('product1', 'this is the first product', 500, 'https://i.postimg.cc/NMMTyd7R/absextract.png', 'produ1', 50)
+
+//console.log(productManager1.getProduct())
+
+// 2DO: no se cargan todos los campos del producto.
+
+productManager1.addProduct('product2', 'this is the second product', 300)
+
+//console.log(productManager1.getProduct())
+
+// 3RO: se carga un code repetido. 
+
+productManager1.addProduct('product3', 'this is the third product', 700, 'https://i.postimg.cc/NMMTyd7R/absextract.png', 'produ1', 57)
+
+//console.log(productManager1.getProduct())
+
+// 4TO: se carga un producto correctamente para comprobar que el ID autoincrementable funciona.
+
+productManager1.addProduct('product4', 'this is the fourth product', 670,'https://i.postimg.cc/NMMTyd7R/absextract.png', 'produ4', 90 )
+
+console.log(productManager1.getProduct())
+
+//5TO: Se buscan dos peoductos por ID.
+
+console.log(productManager1.getProductById(1))
+
+console.log(productManager1.getProductById(4))
